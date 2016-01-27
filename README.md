cb70e0cbedb45a4a6f0432350e04f35352fcfe55
NamJungGu <nowage@gmail.com>
2016-01-27 16:15:57 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# BashShellLibForCentos7  - Introduction
+
+This is useful bash sell scripts for centos7.x
+
+# Version
+
+v0.1
+
+
+# CF        
+This is early version.
+Welcome our code.
+
+# BUGS
+
+Please report bugs to nowage[at]gmail.com.
+
+# todo
+
+password for each node,,,
+download rpm only one time when sshpass install,,,
+
+# CONTRIBUTING
+
+The github repository is at https://github.com/Finfra/BashShellLibForCentos7.git
+
+# SEE ALSO
+
+Some other stuff.
+
+# AUTHOR
+
+NamJungGu, <nowage[at]gmail.com>
+
+# COPYRIGHT AND LICENSE
+
+(c) Copyright 2005-2015 by finfra.com
+
---
+++ ipAutoSet.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+lip=$1
+if [ ${#lip} -eq 0 ];then
+	lip=101
+elif [ $lip -lt 3 ]; then
+	echo 'Can Not use this local-ip.use 3~254'	
+	exit 1
+fi
+
+eth=`nmcli connection show|grep ethernet|awk '{print $1}'`
+nmcli con mod $eth ipv4.method auto
+nmcli con mod $eth ipv4.gateway ""
+nmcli con mod $eth ipv4.addresses ""
+nmcli con down $eth
+nmcli con up $eth
+
+subnet=`ip addr|grep "inet\ "|grep $eth|awk '{print $2}'|awk 'BEGIN{FS="."}{print $1"."$2"."$3}'`
+
+nmcli con up $eth
+nmcli con mod $eth ipv4.dns "168.126.63.1 8.8.8.8"
+#echo nmcli con mod $eth ipv4.gateway "$subnet.2"
+nmcli con mod $eth ipv4.addresses "$subnet.$lip/24"
+#echo nmcli con mod $eth ipv4.addresses "$subnet.$lip/24"
+nmcli con mod $eth ipv4.gateway "$subnet.2"
+#echo nmcli con mod $eth ipv4.gateway "$subnet.2"
+nmcli con mod $eth ipv4.method "manual"
+nmcli con down $eth
+nmcli con up $eth
+
+echo '----------------------------------------------------------------------'
+echo Ip Address is `ip addr|grep "inet\ "|grep $eth|awk '{print  $2}'`
+echo '----------------------------------------------------------------------'
